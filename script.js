051da1a9041ff4e3268a4bfb77d988af5facbd7a
tommasoligi <tommasoligi12@gmail.com>
2026-07-22 03:20:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function sboccia(){
-
-    const fiore = document.querySelector(".fiore");
-    const rosa = document.querySelector(".rosa");
-
-    fiore.style.animation = "apertura 2s forwards";
-
-    rosa.style.animation = "vento 3s infinite";
-
-}
+onload = () => {
+  document.body.classList.remove("container");
+};
