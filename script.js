4a64b866122ccd6b4e548777fdf41e945bd6e1cc
tommasoligi <tommasoligi12@gmail.com>
2026-07-22 03:13:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function sboccia(){
+
+    const fiore = document.querySelector(".fiore");
+    const rosa = document.querySelector(".rosa");
+
+    fiore.style.animation = "apertura 2s forwards";
+
+    rosa.style.animation = "vento 3s infinite";
+
+}
